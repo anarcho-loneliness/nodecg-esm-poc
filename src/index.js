a3d4523b4dcc64ec74064bebd347128ade1744b1
Alex Van Camp <vancamp.alex@gmail.com>
2019-07-06 15:03:45 -0500

    more convincing example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import cjsFunction from 'noop3';
-import esmFunction from '@tomekf/noop';
-import {log} from './nodecg.js';
+import cjsColor from 'rgb'; // CJS dep from npm
+import * as d3 from 'd3-color'; // ESM dep from npm
+import {log} from './nodecg.js'; // external ESM
 
-cjsFunction();
-esmFunction();
+console.log('cjs deps work if this prints an rgb string:', cjsColor('maroon'));
+console.log('esm deps work if this prints an rgb object:', d3.color("steelblue"));
 
 function component() {
 	const element = document.createElement('div');
--- src/parcel.js
+++ src/parcel.js
@@ -1,8 +1,8 @@
-import cjsFunction from 'noop3';
-import esmFunction from '@tomekf/noop';
+import cjsColor from 'rgb'; // CJS dep from npm
+import * as d3 from 'd3-color'; // ESM dep from npm
 
-cjsFunction();
-esmFunction();
+console.log('cjs deps work if this prints an rgb string:', cjsColor('maroon'));
+console.log('esm deps work if this prints an rgb object:', d3.color("steelblue"));
 
 function component() {
 	const element = document.createElement('div');
